9c9aed4ab256baaf4ddcab06ad2d621dbd0d5711
Vyacheslav <informationsss@ya.ru>
2022-03-29 13:52:13 +0300

    Декомпозиция класса MessageDialog

--- src/main/java/frontend/window/optionDialog/MessageDialog.java
+++ src/main/java/frontend/window/optionDialog/MessageDialog.java
@@ -3,35 +3,48 @@ package frontend.window.optionDialog;
 import frontend.controlElement.Label;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MessageDialog {
 
     public static class Error extends MessageDialog {
 
-        public Error(String message) {
+        public Error(Object message) {
             super(message, "Ошибка", JOptionPane.ERROR_MESSAGE);
+        }
+
+        public Error(String message) {
+            this(new Label(message));
         }
     }
 
     public static class Info extends MessageDialog {
 
-        public Info(String message) {
+        public Info(Object message) {
             super(message, "Сообщение", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        public Info(String message) {
+            this(new Label(message));
         }
     }
 
     public static class Warning extends MessageDialog {
 
-        public Warning(String message) {
+        public Warning(Object message) {
             super(message, "Предупреждение", JOptionPane.WARNING_MESSAGE);
+        }
+
+        public Warning(String message) {
+            this(new Label(message));
         }
     }
 
-    private MessageDialog(Object[] message, String title, int messageType) {
-        JOptionPane.showMessageDialog(null, message, title, messageType);
+    public MessageDialog(Component parentComponent, Object message, String title, int messageType, Icon icon) {
+        JOptionPane.showMessageDialog(parentComponent, message, title, messageType, icon);
     }
 
-    private MessageDialog(String message, String title, int messageType) {
-        this(new Object[]{new Label(message)}, title, messageType);
+    public MessageDialog(Object message, String title, int messageType) {
+        this(null, message, title, messageType, null);
     }
 }
